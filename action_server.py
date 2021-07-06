e4f035d9da44022454c0b4eef5de52c15890dae0
Robin Heitz <robin_rob96@me.com>
2021-07-06 19:35:00 +0200

    test changes

--- action_server.py
+++ action_server.py
@@ -6,12 +6,30 @@ import actionlib
 
 import Adafruit_PCA9685
 
-class TestServer:
+import robot_arm_control_pkg.msg
+
+
+class ServoControlAction:
+
+    _feedback = robot_arm_control_pkg.msg.servo_controlFeedback()
+    _result = robot_arm_control_pkg.msg.servo_controlResult()
 
     def __init__(self, name):
         self._action_name = name
-        self._as = actionlib.SimpleActionServer(self._action_name)
+        self._as = actionlib.SimpleActionServer(
+            self._action_name, 
+            robot_arm_control_pkg.msg.servo_controlAction, 
+            execute=self.execute_cb, 
+            auto_start=False,
+            )
+        self._as.start()
 
+    def execute_cb(self, goal):
+        r = rospy.Rate(1)
+        success = True
+
+        self._feedback.angel  = 69
+ 
 
 if __name__ == "__main__":
     rospy.init_node("TestNode")
