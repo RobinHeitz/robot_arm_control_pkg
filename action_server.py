2eccebcab0fb1fd2e7dd30ec218297329327efbe
Robin Heitz <robin_rob96@me.com>
2021-07-08 19:32:51 +0200

    test

--- action_server.py
+++ action_server.py
@@ -18,6 +18,9 @@ def do_action(goal):
     channel = goal.channel
     angle = goal.angle
 
+    print("Start doing goal...")
+
+
     servoController = ServoController(channel)
     servoController.move_servo_to_degree(angle)
     time.sleep(5)
@@ -30,4 +33,5 @@ if __name__ == "__main__":
     rospy.init_node("servo_control_server_node")
     server = actionlib.SimpleActionServer("servo_controller_topic", ServoControlAction, do_action, False)
     server.start()
+    print("rospy spin now...")
     rospy.spin()
