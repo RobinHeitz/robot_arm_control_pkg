2266f0f5ba5b6fce6dbcb3db60eb7aa8eae82d41
Robin Heitz <robin_rob96@me.com>
2021-07-09 09:49:35 +0000

    trying stuff out

--- action_server.py
+++ action_server.py
@@ -14,24 +14,37 @@ from scripts.servo_control_pkg.servo_controller import ServoController, ServoCon
 
 
 
-def do_action(goal):
+def move_servo(goal):
     channel = goal.channel
     angle = goal.angle
 
     print("Start doing goal...")
 
+    servoController = ServoController()
 
-    servoController = ServoController(channel)
-    servoController.move_servo_to_degree(angle)
-    time.sleep(5)
-    print("FINISHED MOVEMENT")
-    result = ServoControlResult()
-    result.angle = 69
-    server.set_succeeded(result)
+    try:
+        servoController.move_servo_to_degree(angle, channel)
+    
+    except ServoControllerFinishedMovementException as e:
+        print(e)
+        print("FINISHED MOVEMENT" )
+        result = ServoControlResult()
+        result.angle = 69
+        server.set_succeeded(result)
+        return
+
+    
+    
+
+    
+    time.sleep(15)
+    print("Cancel goal")
+    server.cancel_all_goals()
+
 
 if __name__ == "__main__":
     rospy.init_node("servo_control_server_node")
-    server = actionlib.SimpleActionServer("servo_controller_topic", ServoControlAction, do_action, False)
+    server = actionlib.SimpleActionServer("servo_controller_topic", ServoControlAction, move_servo, False)
     server.start()
     print("rospy spin now...")
     rospy.spin()
