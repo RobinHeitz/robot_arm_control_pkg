192d386b0795a7e037bccd0de30f84fe07310904
Robin Heitz <robin_rob96@me.com>
2021-07-30 10:37:31 +0200

    changes for testing actions

--- action_client.py
+++ action_client.py
@@ -3,6 +3,14 @@ import rospy
 import actionlib
 from robot_arm_control_pkg.msg import ServoControlResult, ServoControlFeedback, ServoControlAction, ServoControlGoal
 
+
+def feedback_cb(feedback):
+    print('We got into feedback cb')
+    print(feedback)
+
+
+
+
 rospy.init_node("servo_control_client_node")
 client = actionlib.SimpleActionClient("servo_controller_topic", ServoControlAction)
 client.wait_for_server()
@@ -11,7 +19,7 @@ goal = ServoControlGoal()
 goal.angle=123.456
 goal.channel=789
 
-client.send_goal(goal)
+client.send_goal(goal, feedback_cb=feedback_cb)
 print("GOAL WAS SENT FROM CLIENT")
 client.wait_for_result()
 
--- action_server.py
+++ action_server.py
@@ -11,23 +11,38 @@ from robot_arm_control_pkg.msg import ServoControlAction, ServoControlGoal, Serv
 from scripts.servo_controller import ServoController, ServoControllerFinishedMovementException
 
 
-
-
-
 def do_action(goal):
+
+    print("do_action_callback")
     channel = goal.channel
     angle = goal.angle
+    print('channel: {} angle:{}'.format(channel, angle))
 
-    print("Start doing goal...")
-
-
-    servoController = ServoController(channel)
-    servoController.move_servo_to_degree(angle)
+    feedback = ServoControlFeedback()
+    feedback.angle = 123
+    server.publish_feedback(feedback)
     time.sleep(5)
-    print("FINISHED MOVEMENT")
     result = ServoControlResult()
-    result.angle = 69
+    result.angle = 456
     server.set_succeeded(result)
+
+
+
+    
+# def do_action(goal):
+#     channel = goal.channel
+#     angle = goal.angle
+
+#     print("Start doing goal...")
+
+
+#     servoController = ServoController(channel)
+#     servoController.move_servo_to_degree(angle)
+#     time.sleep(5)
+#     print("FINISHED MOVEMENT")
+#     result = ServoControlResult()
+#     result.angle = 69
+#     server.set_succeeded(result)
 
 if __name__ == "__main__":
     rospy.init_node("servo_control_server_node")
